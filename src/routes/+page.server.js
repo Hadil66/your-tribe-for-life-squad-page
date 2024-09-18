99b296b8ace220a37272e9d2dc37194fc5277e32
SamaraFellaDina <sammybensink@gmail.com>
2024-09-18 22:52:04 +0200

    ophalen van gefilterde data door mapping #15

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,7 +3,10 @@ import fetchJson from "$lib/fetch-json"
 export async function load() {
   const url = 'https://fdnd.directus.app/items/person/'
 
-  const persons = await fetchJson(url)
+  const persons = await fetchJson(url);
+
+  var personAvatars = persons.data.map(person => person.avatar);
+  console.log(personAvatars);
 
   return {
     persons: persons.data
